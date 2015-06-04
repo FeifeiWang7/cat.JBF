f804883952b209fd96f7eaaa3f3f6dcdaff94d8a
FeifeiWang7 <fwang12@ncsu.edu>
2015-06-04 11:53:54 +0800

    Update string_to_int.c

--- pre/library/string_to_int.c
+++ pre/library/string_to_int.c
@@ -1,8 +1,8 @@
 int string_to_int(char num[])
 {
         //consider integer overflow. Below assume 32-bit integer
-        if((num[0] == '-') && (strcmp (num, "-2147483648") > 0)) return 0;
-        else if(strcmp(num, "2147483647") > 0) return 0;
+        if((num[0] == '-') && (strlen(num) == strlen("-2147483648")) && (strcmp (num, "-2147483648") > 0)) return 0;
+        else if( (strlen(num) == strlen("2147483647")) && (strcmp(num, "2147483647") > 0)) return 0;
         int len = strlen(num);
         int result = 0;
         int i = 0;
